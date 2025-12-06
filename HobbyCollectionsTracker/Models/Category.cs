416e3e283f665e57679ba9a7db7387267e961237
Erick Moura <erick.andrew.moura@gmail.com>
2025-12-06 18:46:13 -0300

    "Adding Summary"

--- HobbyCollectionsTracker/Models/Category.cs
+++ HobbyCollectionsTracker/Models/Category.cs
@@ -4,10 +4,19 @@ namespace HobbyCollectionsTracker.Models;
 
 public class Category
 {
+  /// <summary>
+  /// Key Indentification oof the Category
+  /// </summary>
   public int Id { get; set; }
-  
+
+  /// <summary>
+  /// Category Name
+  /// </summary>
   [Required, StringLength(50, ErrorMessage = "Name must be 50 characters or less.")]
   public string Name { get; set; } = string.Empty;
 
+  /// <summary>
+  /// List of the Post using this category
+  /// </summary>
   public ICollection<Post> posts { get; set; } = new List<Post>();
 }
--- HobbyCollectionsTracker/Models/Post.cs
+++ HobbyCollectionsTracker/Models/Post.cs
@@ -5,28 +5,43 @@ namespace HobbyCollectionsTracker.Models;
 
 public class Post
 {
+  /// <summary>
+  /// Primary key for the post
+  /// </summary>
   public int Id { get; set; }
 
+  /// <summary>
+  /// Post Title
+  /// </summary>
   [Required, StringLength(100, ErrorMessage = "Title must be 100 characters or less.")]
   public string Title { get; set; } = string.Empty; //
 
+  /// <summary>
+  /// Post Description
+  /// </summary>
   [Required, StringLength(2000, ErrorMessage = "Description must be 2000 characters or less.")]
   public string Description { get; set; } = string.Empty; //
 
+  // Post image (that is optional)
   [Url]
   [Display(Name = "Image URL")]
   public string? ImageUrl { get; set; }
 
+  /// <summary>
+  /// Post Creation data 
+  /// </summary>
   [Required, Display(Name = "Created At")]
   public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-  // Relation with Category Model
+  
+  /// <summary>
+  /// Category Id of the Category Selected for the post
+  /// </summary>
   [Required, Display(Name = "Category")]
   public int CategoryId { get; set; }
   public Category? Category { get; set; } 
 
-  // Relation with User
+  /// <summary>
+  /// Key Id of the user who created the post
+  /// </summary>
   public string UserId { get; set; } = string.Empty;
-
-  // public IdentityUser? User { get; set; }
 }
